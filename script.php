5fc26cb6e6acc7c49654d8b6938f90a73ecb4a3b
Caelen Cater <64516705+caelen-cater@users.noreply.github.com>
2024-01-05 09:43:54 -0600

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,9 @@
 <?php
 function processUPC($upc, $userId = 0) {
+    if ($userId <= 0) {
+        return ['error' => 'User ID must be greater than 0'];
+    }
+
     $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http";
     $domain = "$protocol://$_SERVER[HTTP_HOST]";
     $apiKey = "your_api_key"; // replace with your actual API key
@@ -11,12 +15,19 @@ function processUPC($upc, $userId = 0) {
     $category = $upcData['brandedFoodCategory'];
     $labelNutrients = $upcData['labelNutrients'];
 
-    $userScriptPath = "user/" . $userId . "/script.php";
-    if (!file_exists($userScriptPath)) {
-        $encryption_key = bin2hex(openssl_random_pseudo_bytes(16));
-        $defaultScript = "<?php\n\n\$data = \$_GET;\n\$encryptedData = openssl_encrypt(json_encode(\$data), 'AES-256-CBC', '$encryption_key', 0, substr('$encryption_key', 0, 16));\nfile_put_contents('log.txt', \$encryptedData);\n\n?>";
-        file_put_contents($userScriptPath, $defaultScript);
+    $date = date('Y-m-d');
+    $userFolderPath = "user/" . $userId;
+    $dateFolderPath = $userFolderPath . "/" . $date;
+    if (!file_exists($dateFolderPath)) {
+        mkdir($dateFolderPath, 0777, true);
     }
+
+    $userScriptPath = $userFolderPath . "/script.php";
+        if (!file_exists($userScriptPath)) {
+            $encryption_key = bin2hex(openssl_random_pseudo_bytes(16));
+            $defaultScript = "<?php\n\n\$data = \$_GET;\n\$date = date('Y-m-d');\n\$dateFolderPath = __DIR__ . '/' . \$date;\nif (!file_exists(\$dateFolderPath)) {\n    mkdir(\$dateFolderPath, 0777, true);\n}\n\$encryptedData = base64_encode(openssl_encrypt(json_encode(\$data), 'AES-256-CBC', '$encryption_key', 0, substr('$encryption_key', 0, 16)));\nfile_put_contents(\$dateFolderPath . '/log.txt', \$encryptedData);\n\n?>";
+            file_put_contents($userScriptPath, $defaultScript);
+        }
 
     $userScriptUrl = $domain . "/" . $userScriptPath . "?description=" . urlencode($description) . "&category=" . urlencode($category) . "&labelNutrients=" . urlencode(json_encode($labelNutrients));
     $userScriptResponse = file_get_contents($userScriptUrl);
