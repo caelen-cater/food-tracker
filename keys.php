0f4a06a0892eb3ab73a926513172d07deb297bd3
Caelen Cater <64516705+caelen-cater@users.noreply.github.com>
2024-01-05 12:46:44 -0600

    Added security feature

--- keys.php
+++ keys.php
@@ -1,3 +1,11 @@
-script=script.php_api_key
-read=read.php_api_key
-fda=WYNFR1AFXFUGh684FZWakn41WDSKX4gJsM3CyQys
+<?php
+if (empty(debug_backtrace())) {
+    echo '<h1>Missing Access</h1>';
+    exit;
+}
+
+return [
+    'script' => 'script.php_api_key',
+    'read' => 'read.php_api_key',
+    'fda' => 'WYNFR1AFXFUGh684FZWakn41WDSKX4gJsM3CyQys',
+];
